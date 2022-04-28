1d3dc2f062c3c3ce9c82eaef7b39620b00b540d0
mangohow <90433169+mangohow@users.noreply.github.com>
2022-04-28 21:24:20 +0800

    add Decorate

--- golang_web/router/router.go
+++ golang_web/router/router.go
@@ -7,108 +7,14 @@ import (
 
 type Handler func(ctx *gin.Context) *response.Response
 
-type Engine struct {
-	*gin.Engine
-}
-
-func NewEngine(e *gin.Engine) *Engine {
-	return &Engine{e}
-}
-
-type RouterGroup struct {
-	*gin.RouterGroup
-}
-
-func NewRouterGroup(r *gin.RouterGroup) *RouterGroup {
-	return &RouterGroup{r}
-}
-
-func (e *Engine) Get(relativePath string, handler Handler) {
-	e.GET(relativePath, func(ctx *gin.Context) {
-		r := handler(ctx)
+// 装饰器
+func Decorate(h Handler) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		r := h(ctx)
 		if r != nil {
 			ctx.JSON(r.HttpStatus, &r.R)
 		}
 
 		response.PutResponse(r)
-	})
-}
-
-func (e *Engine) Post(relativePath string, handler Handler) {
-	e.POST(relativePath, func(ctx *gin.Context) {
-		r := handler(ctx)
-		if r != nil {
-			ctx.JSON(r.HttpStatus, &r.R)
-		}
-
-		response.PutResponse(r)
-	})
-}
-
-func (e *Engine) Put(relativePath string, handler Handler) {
-	e.PUT(relativePath, func(ctx *gin.Context) {
-		r := handler(ctx)
-		if r != nil {
-			ctx.JSON(r.HttpStatus, &r.R)
-		}
-
-		response.PutResponse(r)
-	})
-}
-
-func (e *Engine) Delete(relativePath string, handler Handler) {
-	e.DELETE(relativePath, func(ctx *gin.Context) {
-		r := handler(ctx)
-		if r != nil {
-			ctx.JSON(r.HttpStatus, &r.R)
-		}
-
-		response.PutResponse(r)
-	})
-}
-
-
-
-func (r *RouterGroup) Get(relativePath string, handler Handler) {
-	r.GET(relativePath, func(ctx *gin.Context) {
-		r := handler(ctx)
-		if r != nil {
-			ctx.JSON(r.HttpStatus, &r.R)
-		}
-
-		response.PutResponse(r)
-	})
-}
-
-func (r *RouterGroup) Post(relativePath string, handler Handler) {
-	r.POST(relativePath, func(ctx *gin.Context) {
-		r := handler(ctx)
-		if r != nil {
-			ctx.JSON(r.HttpStatus, &r.R)
-		}
-
-		response.PutResponse(r)
-	})
-}
-
-func (r *RouterGroup) Put(relativePath string, handler Handler) {
-	r.PUT(relativePath, func(ctx *gin.Context) {
-		r := handler(ctx)
-		if r != nil {
-			ctx.JSON(r.HttpStatus, &r.R)
-		}
-
-		response.PutResponse(r)
-	})
-}
-
-func (r *RouterGroup) Delete(relativePath string, handler Handler) {
-	r.DELETE(relativePath, func(ctx *gin.Context) {
-		r := handler(ctx)
-		if r != nil {
-			ctx.JSON(r.HttpStatus, &r.R)
-		}
-
-		response.PutResponse(r)
-	})
+	}
 }
